360de12b6b28c0abbfc10dab1244d5fe2d3e808a
Mohamed hussien <m.hussien.m44@gmail.com>
2025-05-06 17:41:44 +0300

    add refresh button

--- app/src/main/java/com/personal/myapplication/ui/home/HomeScreen.kt
+++ app/src/main/java/com/personal/myapplication/ui/home/HomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -28,7 +29,6 @@ fun HomeScreen(modifier: Modifier = Modifier) {
     }
 
     Column(modifier.fillMaxSize()) {
-
         LazyColumn {
             items(state.photos){
                 AsyncImage(
@@ -42,5 +42,12 @@ fun HomeScreen(modifier: Modifier = Modifier) {
         if (state.errorMessage.isNotEmpty())
             Text(state.errorMessage)
 
+
+        Button(onClick ={
+            viewModel.getPhotos()
+        }) {
+
+            Text("Refresh")
+        }
     }
 }
