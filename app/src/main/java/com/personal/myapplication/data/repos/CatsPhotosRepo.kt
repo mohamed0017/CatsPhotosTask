7a466f6c3f2dcf8821d8ffc9cee36dcc04832481
Mohamed hussien <m.hussien.m44@gmail.com>
2025-05-06 17:39:50 +0300

    api request error handle

--- app/src/main/java/com/personal/myapplication/data/repos/CatsPhotosRepo.kt
+++ app/src/main/java/com/personal/myapplication/data/repos/CatsPhotosRepo.kt
@@ -3,34 +3,37 @@ package com.personal.myapplication.data.repos
 import com.personal.myapplication.data.CatsPhotosApi
 import com.personal.myapplication.data.CatsPhotosResponse
 import com.personal.myapplication.domain.repos.CatsPhotosRepo
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
-import retrofit2.Retrofit
 import javax.inject.Inject
-import javax.inject.Singleton
 
-class CatsPhotosRepoImpl @Inject constructor(private val catsPhotosApi: CatsPhotosApi) : CatsPhotosRepo {
+class CatsPhotosRepoImpl @Inject constructor(private val catsPhotosApi: CatsPhotosApi) :
+    CatsPhotosRepo {
     override suspend fun getPhotos(): Result<List<CatsPhotosResponse>> {
-        val response = catsPhotosApi.getCatsPhotos()
 
-        return if (response.isSuccessful) {
-            Result.success(response.body() ?: emptyList())
-        } else {
+        try {
+            val response = catsPhotosApi.getCatsPhotos()
 
-            return when (response.code()) {
-                404 -> {
-                    Result.failure(Exception())
+            return if (response.isSuccessful) {
+                Result.success(response.body() ?: emptyList())
+            } else {
+
+                return when (response.code()) {
+                    404 -> {
+                        Result.failure(Exception("Error: 404 Not Found"))
+                    }
+
+                    500 -> {
+                        Result.failure(Exception("Error: 500 Server Error"))
+                    }
+
+                    else -> {
+                        Result.failure(Exception("Error:  Unknown Error"))
+                    }
                 }
 
-                500 -> {
-                    Result.failure(Exception())
-                }
-
-                else -> {
-                    Result.failure(Exception())
-                }
             }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return Result.failure(Exception("No Internet"))
 
         }
     }
